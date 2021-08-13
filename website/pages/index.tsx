ebbd8acd98d5ecd33652abaaca5e89ad1459a4ba
kishek2000 <kishek12@gmail.com>
2021-08-13 10:11:24 +1000

    feat: initial commit

---
+++ website/pages/index.tsx
@@ -0,0 +1,18 @@
+/** @jsxImportSource @emotion/react */
+
+export default function Home() {
+  return (
+    <div
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100vw',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <h1 css={{ fontFamily: 'Oswald', fontWeight: 200 }}>Coming Soon...</h1>
+    </div>
+  );
+}
